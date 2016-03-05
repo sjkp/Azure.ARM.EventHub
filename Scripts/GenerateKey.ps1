75e0090c0028d8f561a4d10ebaa2dd8fbec38696
Simon J.K. Pedersen <mail@sjkp.dk>
2016-03-05 13:11:04 +0100

    initial

---
+++ Scripts/GenerateKey.ps1
@@ -0,0 +1,9 @@
+﻿#
+# GenerateKey.ps1
+#
+$bytes = New-Object Byte[] 32
+$rand = [System.Security.Cryptography.RandomNumberGenerator]::Create()
+$rand.GetBytes($bytes)
+$rand.Dispose()
+$key = [System.Convert]::ToBase64String($bytes)
+Write-Host $key
